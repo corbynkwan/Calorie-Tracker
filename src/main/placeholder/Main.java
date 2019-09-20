785bd002ed901e6285c9c063b6a6d10be37c8d07
Corbyn <naruto01@students.cs.ubc.ca>
2019-09-20 16:10:03 -0700

    Project 2

--- src/main/placeholder/Main.java
+++ src/main/placeholder/Main.java
@@ -1,7 +1,33 @@
 package placeholder;
 
+import ui.Food;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        Food f = new Food();
+        f.setProtein(50.0);
+        f.setCarbs(10);
+        f.setFats(0);
+
+        if (f.getProtein() >= 30) {
+            System.out.println("This food has a lot of protein");
+        }
+
+        String input = "yes";
+        while (input.equals("yes")) {
+            System.out.print("Do you want to know the nutritional facts of chicken breast(100g)?");
+            input = in.nextLine();
+            if(input.equals("yes")) {
+                System.out.println(f.getProtein());
+                System.out.println(f.getCarbs());
+                System.out.println(f.getFats());
+            }
+        }
 
     }
 }
